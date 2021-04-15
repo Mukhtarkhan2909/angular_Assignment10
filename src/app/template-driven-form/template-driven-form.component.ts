cb46b2b50d839058a25cfd92382fcf265e82d2fc
Mukhtarkhan2000 <m.nalibayev2900@gmail.com>
2021-04-15 23:25:23 +0600

    Initial commit

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -11,8 +11,7 @@ export class TemplateDrivenFormComponent implements OnInit {
 
   constructor() { }
 
-  // @ts-ignore
-  @ViewChild('templateDrivenForm', null)
+  @ViewChild('templateDrivenForm')
   templateDrivenForm: NgForm | undefined;
 
   contact: Contact | undefined;
@@ -51,6 +50,61 @@ export class TemplateDrivenFormComponent implements OnInit {
 
   onSubmit(templateDrivenForm: NgForm): void {
     console.log(templateDrivenForm.value);
+  }
+
+  setDefaults(): void {
+    // @ts-ignore
+    this.templateDrivenForm.setValue(this.contact);
+  }
+
+  changeFirstName(): void {
+    // @ts-ignore
+    this.templateDrivenForm.controls.firstName.setValue('Johnny');
+  }
+
+  patchValue(): void {
+    const obj = {
+      form_1: {
+        form_2: {
+          form_2_1: {
+            firstName: 'New name'
+          },
+          form_2_2: {
+            lastName: 'New surname'
+          }
+        }
+      }
+    };
+
+    // @ts-ignore
+    this.templateDrivenForm.control.patchValue(obj);
+
+  }
+
+  changeEmail(): void {
+    const obj = {
+      form_2: {
+        form_3: {
+          form_3_1: {
+            email: 'new_email@mail.com'
+          }
+        }
+      }
+    };
+    // @ts-ignore
+    const address = this.templateDrivenForm.controls.form_1 as FormGroup;
+    address.patchValue(obj);
+
+  }
+
+  reset(): void {
+    // @ts-ignore
+    this.templateDrivenForm.reset();
+  }
+
+  resetForm(): void {
+    // @ts-ignore
+    this.templateDrivenForm.resetForm();
   }
 
 }
